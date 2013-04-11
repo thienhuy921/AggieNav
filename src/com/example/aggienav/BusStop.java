d0de02ed42ff69fe42be7df14fc2885262746783
qantravon <qantravon@gmail.com>
2013-04-10 22:57:06 -0500

    Added timeToNext to BusStop Made Route a looping linked-list Set up test capabilities; added test for Route functionality

--- src/com/example/aggienav/BusStop.java
+++ src/com/example/aggienav/BusStop.java
@@ -1,6 +1,7 @@
 package com.example.aggienav;
 
 import com.google.android.maps.GeoPoint;
+import java.util.Date;
 
 // Bus Stop is a node in a linked list which represents a Bus route.
 public class BusStop {
@@ -9,6 +10,7 @@ public class BusStop {
 	private double lat;
 	private double lon;
 	private BusStop nextBusStop;
+	private int timeToNext; //time to next stop in seconds
 	
 	public BusStop(){ // Constructor
 		name = "";
@@ -16,20 +18,39 @@ public class BusStop {
 		lon = 0;
 		lat = 0;
 		nextBusStop = null;
+		timeToNext = 0;
 	}
 	public BusStop(double Lat, double Lon,String Name){ // Constructor
 		name = Name;
 		//loc = new GeoPoint(Lon, Lat);
 		lon = Lon;
 		lat = Lat;
-		nextBusStop = null;		
+		nextBusStop = null;
+		timeToNext = 0;
 	}
 	public BusStop(double Lat, double Lon,String Name, BusStop _nextBusStop){ // Constructor
 		name = Name;
 		//loc = new GeoPoint(Lon, Lat);
 		lon = Lon;
 		lat = Lat;
-		nextBusStop = _nextBusStop;		
+		nextBusStop = _nextBusStop;
+		timeToNext = 0;
+	}
+	public BusStop(double Lat, double Lon,String Name, int _timeToNext){ // Constructor
+		name = Name;
+		//loc = new GeoPoint(Lon, Lat);
+		lon = Lon;
+		lat = Lat;
+		nextBusStop = null;
+		timeToNext = _timeToNext;
+	}
+	public BusStop(double Lat, double Lon,String Name, BusStop _nextBusStop, int _timeToNext){ // Constructor
+		name = Name;
+		//loc = new GeoPoint(Lon, Lat);
+		lon = Lon;
+		lat = Lat;
+		nextBusStop = _nextBusStop;
+		timeToNext = _timeToNext;
 	}
 	public double getLon(){
 		return lon;
@@ -43,12 +64,30 @@ public class BusStop {
 	public BusStop getNext(){
 		return nextBusStop;
 	}
+	public int getTimeToNext(){
+		return timeToNext;
+	}
 	public void setNext(BusStop _busStop){
 		nextBusStop = _busStop;
 	}
 	
 	public void display(){
 		System.out.println("Bus Stop name: "+ name +" lat:"+ lat +" lon:"+ lon );
+	}
+	
+	public boolean equals(BusStop _other){
+		if((this.lat == _other.getLat())
+			&& (this.lon == _other.getLon())
+			&& (this.name.equals(_other.getName()))
+			&& (this.timeToNext == _other.getTimeToNext()))
+		{
+			if(this.nextBusStop != null
+				&& this.nextBusStop.equals(_other.getNext()))
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
