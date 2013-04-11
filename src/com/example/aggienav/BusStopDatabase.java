1bb8d1c6b22d41532ab1b29d4aff5b0bcd439489
smdawkins27 <smdawkins27@gmail.com>
2013-04-11 11:00:05 -0500

    reading excel file

--- src/com/example/aggienav/BusStopDatabase.java
+++ src/com/example/aggienav/BusStopDatabase.java
@@ -1,5 +1,43 @@
 package com.example.aggienav;
+import jxl.*;
+import java.io.*;
 
-public class BusStopDatabase {
+public class BusStopDatabase extends BusStop{
+	BusStop[]BSDB = new BusStop[100];
+	private void readExcelSheet(String destFile){ 
+	      File excelSheet = null;
+	      Workbook workbook = null;    
+	           
+	        try {
+	             Workbook wb = Workbook.getWorkbook(new File(destFile));
+	             System.out.println(wb.getNumberOfSheets());
+	             for(int sheetNo=0; sheetNo<wb.getNumberOfSheets();sheetNo++)
+	                {                
+	                  Sheet sheet = wb.getSheet(sheetNo);
+	                  int columns = sheet.getColumns();
+	                  int rows = sheet.getRows();
+	                  String data;
+	                  System.out.println("Sheet Name\t"+wb.getSheet(sheetNo).getName());
+	                          for(int row = 0;row < rows;row++) {
+	                              for(int col = 0;col < columns;col++) {
+	                               data = sheet.getCell(col, row).getContents();
+	                              System.out.print(data+ " ");
+	                            
+	                          }                          
+	                          System.out.println("\n");
+	                      }
+	                      }
+	                  } catch(Exception ioe) {
+	                       ioe.printStackTrace();
+	                  }
+	             
+	          }
+	      public static void main(String arg[]){
+	            //ReadExcel excel = new ReadExcel();
+	            //excel.readExcelSheet("C://Employee.xls");
+	      }
+	      
+	}
 
-}
+
+
